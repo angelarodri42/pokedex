5247c2b4929671f82678125974aa6726e8e87d31
sebinko <halas.sebastian@gmail.com>
2025-03-27 10:14:27 +0100

    i guess deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.BASE_URL || '/',  // Use BASE_URL from env or default to '/'
 })
