ee08a16f770104d31901c439861d8e494c370db8
sebinko <halas.sebastian@gmail.com>
2025-03-27 10:50:58 +0100

    test

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,6 @@ import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-// Get base URL from the environment
-const basename = import.meta.env.BASE_URL;
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
