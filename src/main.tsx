d14917a025db63537cbb9ac51bb19e901dd1d99f
sebinko <halas.sebastian@gmail.com>
2025-03-27 10:25:15 +0100

    test

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+// Get base URL from the environment or default to '/pokedex/'
+const basename = import.meta.env.BASE_URL;
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
